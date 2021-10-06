739972d527c1f31059b2487cb02e85d38879b665
sams4566 <sams4566@gmail.com>
2021-10-06 18:46:11 +0000

    ADd game board and option for the user to choose the game type

--- run.py
+++ run.py
@@ -1,15 +1,33 @@
 from pprint import pprint
 from random import randint
 
-def computer_board(size):
-    game_board = [["." for x in range(size)] for y in range(size)]
+
+def start_game():
+    print("Welcome to Battleships!")
+    player_name = input("Please enter your name:\n")
+    choose_game_type()
+    
+
+def choose_game_type():
+    rows = int(input("Please type the number of rows you would like to play with:\n"))
+    columns = int(input("Please type the number of columns you would like to play with:\n"))
+    computer_board(rows, columns)
+
+def computer_board(rows, columns):
+    game_board = [["." for x in range(rows)] for y in range(columns)]
     for row in game_board:
             print(" ".join(row))
 
-def user_board(size):
-    game_board = [["." for x in range(size)] for y in range(size)]
-    for row in game_board:
-            print(" ".join(row))
+def main():
+    start_game()
+
+main()
+
+
+# def user_board(size):
+#     game_board = [["." for x in range(size)] for y in range(size)]
+#     for row in game_board:
+#             print(" ".join(row))
 
 # def user_choice():
 
@@ -22,12 +40,6 @@ def user_board(size):
 # def user_winner():
 
 # def computer_winner():
-
-def main():
-    computer_board(10)
-    user_board(10)
-
-main()
 
 
 
