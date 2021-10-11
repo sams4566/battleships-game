1019158dd2a4bb1be745ca3961ecf38ff05f7fd5
sams4566 <sams4566@gmail.com>
2021-10-11 13:29:44 +0000

    Add error messages if incorrect keys are inputed

--- run.py
+++ run.py
@@ -81,36 +81,73 @@ def calculate_ships(game_board, rows, columns):
     return game_board
 
 def users_choice(board_two, rows, SCORE1):
-    user_row1 = int(input("Please type the first number of your coordinate:\n"))
-    user_column1 = int(input("Please type the second number of your coordinate:\n"))
+    
+    while True:
+        user_row1 = input("Please type the first number of your coordinate:\n")
 
-    if user_row1 < rows and user_row1 >= 0 and user_column1 < rows and user_column1 >= 0:
-        print("Hello there")
-        if board_two[user_row1][user_column1] == ".":
-            board_two[user_row1][user_column1] = "M"
-            
-        elif board_two[user_row1][user_column1] == '#':
-            board_two[user_row1][user_column1] = "X"
-            SCORE1 = SCORE1 + 1
-            print(f"This is the correct {SCORE1}")
-            
-            # SCORE1 = scores1(SCORE1, rows)
-        elif board_two[user_row1][user_column1] == "M":
-            print("You have already chosen that coordinate, please choose again!")
-            SCORE1 = SCORE1
-            users_choice(board_two, rows, SCORE1)
-            
-        elif board_two[user_row1][user_column1] == "X":
-            print("You have already chosen that coordinate, please choose again!")
-            SCORE1 = SCORE1
-            users_choice(board_two, rows, SCORE1)
-            
+        if correct_input1(user_row1, rows):
+            break
 
-    elif user_row1 != int:
-        print(f"Please choose a number between 0 - {rows - 1}")
+    while True:
+        user_column1 = input("Please type the second number of your coordinate:\n")
+
+        if correct_input2(user_column1, rows):
+            break
+
+    user_row1 = int(user_row1)
+    user_column1 = int(user_column1)
+
+    # if user_row1 < rows and user_row1 >= 0 and user_column1 < rows and user_column1 >= 0:
+    print("Hello there")
+    if board_two[user_row1][user_column1] == ".":
+        board_two[user_row1][user_column1] = "M"
+        
+    elif board_two[user_row1][user_column1] == '#':
+        board_two[user_row1][user_column1] = "X"
+        SCORE1 = SCORE1 + 1
+        print(f"This is the correct {SCORE1}")
+        
+        # SCORE1 = scores1(SCORE1, rows)
+    elif board_two[user_row1][user_column1] == "M":
+        print("You have already chosen that coordinate, please choose again!")
+        SCORE1 = SCORE1
+        users_choice(board_two, rows, SCORE1)
+        
+    elif board_two[user_row1][user_column1] == "X":
+        print("You have already chosen that coordinate, please choose again!")
         SCORE1 = SCORE1
         users_choice(board_two, rows, SCORE1)
     return board_two, SCORE1
+            
+
+    # elif user_row1 != int or float or user_column1 != int or float:
+    #     print(f"Please choose a number between 0 - {rows - 1}")
+    #     SCORE1 = SCORE1
+    #     users_choice(board_two, rows, SCORE1)
+
+def correct_input1(user_row1, rows):
+    try:
+        if int(user_row1) > rows or int(user_row1) < 0:
+            raise ValueError(
+                f"Please choose a number between 0 - {rows - 1}"
+            )
+    except ValueError as e:
+        print(f"Invalid entry: {e}, please choose a number between 0 - {rows - 1}\n")
+        return False
+    
+    return True
+
+def correct_input2(user_column1, rows):
+    try:
+        if int(user_column1) > rows or int(user_column1) < 0:
+            raise ValueError(
+                f"Please choose a number between 0 - {rows - 1}"
+            )
+    except ValueError as e:
+        print(f"Invalid entry: {e}, please choose a number between 0 - {rows - 1}\n")
+        return False
+    
+    return True
 
 def computer_choice(board_one, rows, columns, SCORE2):
     user_row = int(random.randrange(rows))
@@ -133,25 +170,27 @@ def scores2(SCORE2, rows):
 def print_board(board, rows):
     print(' ', end=" ")
     print(f'{nums_top(rows)}')
-
+    
     for row, x in zip(board, range(rows)):
         print(f'{x}| ' + '   '.join(row) + ' |')
+
     print(' ' + ' ‾' * rows * 2)
 
 def print_board2(board, rows):
-    
-    for x in range(rows):
-        for y in range(rows):
-            if board[x][y] == "#":
-                board[x][y] = "@"
 
     print("The is print_board2") 
 
     print(' ', end=" ")
     print(f'{nums_top(rows)}')
 
+    for x in range(rows):
+        for y in range(rows):
+            if board[x][y] == "#":
+                board[x][y] = "@"
+
     for row, x in zip(board, range(rows)):
         print(f'{x}| ' + '   '.join(row) + ' |')
+
     print(' ' + ' ‾' * rows * 2)
     
 def nums_top(rows):
