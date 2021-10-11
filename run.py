c6f800e540e76abe3932f7a4446d451616701dc9
sams4566 <sams4566@gmail.com>
2021-10-11 11:24:20 +0000

    Add top row of numbers for all game types

--- run.py
+++ run.py
@@ -14,14 +14,22 @@ def choose_game_type():
     board_two = computer_board(rows, columns)
     SCORE1 = 0
     SCORE2 = 0
+    user_row1 = 0
+    user_column1 = 0
     
     for x in range(100):
         board_one, SCORE2 = computer_choice(board_one, rows, columns, SCORE2)
         board_two, SCORE1 = users_choice(board_two, rows, SCORE1)
     
 
+        print(f"This is your score:{SCORE1}")
+        print(f"This is the Computers score:{SCORE2}")
+
         print_board(board_one, rows)
         print("Your Board")
+        print(f"Computer chose coordinates {user_row1}, {user_column1}")
+
+
         print_board2(board_two, rows)
         print("Computer's Board")
 
@@ -73,31 +81,36 @@ def calculate_ships(game_board, rows, columns):
     return game_board
 
 def users_choice(board_two, rows, SCORE1):
-    user_row = int(input("Please type the first number of your coordinate:\n"))
-    user_column = int(input("Please type the second number of your coordinate:\n"))
+    user_row1 = int(input("Please type the first number of your coordinate:\n"))
+    user_column1 = int(input("Please type the second number of your coordinate:\n"))
 
-    if user_row < rows and user_row >= 0 and user_column < rows and user_column >= 0:
+    if user_row1 < rows and user_row1 >= 0 and user_column1 < rows and user_column1 >= 0:
         print("Hello there")
-        if board_two[user_row][user_column] == ".":
-            board_two[user_row][user_column] = "M"
-        elif board_two[user_row][user_column] == '#':
-            board_two[user_row][user_column] = "X"
-            SCORE1 = scores1(SCORE1, rows)
-        elif board_two[user_row][user_column] == "M":
+        if board_two[user_row1][user_column1] == ".":
+            board_two[user_row1][user_column1] = "M"
+            
+        elif board_two[user_row1][user_column1] == '#':
+            board_two[user_row1][user_column1] = "X"
+            SCORE1 = SCORE1 + 1
+            print(f"This is the correct {SCORE1}")
+            
+            # SCORE1 = scores1(SCORE1, rows)
+        elif board_two[user_row1][user_column1] == "M":
             print("You have already chosen that coordinate, please choose again!")
+            SCORE1 = SCORE1
             users_choice(board_two, rows, SCORE1)
-        elif board_two[user_row][user_column] == "X":
+            
+        elif board_two[user_row1][user_column1] == "X":
             print("You have already chosen that coordinate, please choose again!")
+            SCORE1 = SCORE1
             users_choice(board_two, rows, SCORE1)
-        return board_two, SCORE1
-        
-    elif user_row != int:
-        print(f"Please choose a number between 0 - {rows - 1}")
-        users_choice(board_two, rows, SCORE1)
+            
 
-def scores1(SCORE1, rows):
-    SCORE1 = SCORE1 + 1
-    return SCORE1
+    elif user_row1 != int:
+        print(f"Please choose a number between 0 - {rows - 1}")
+        SCORE1 = SCORE1
+        users_choice(board_two, rows, SCORE1)
+    return board_two, SCORE1
 
 def computer_choice(board_one, rows, columns, SCORE2):
     user_row = int(random.randrange(rows))
@@ -118,7 +131,8 @@ def scores2(SCORE2, rows):
     return SCORE2
 
 def print_board(board, rows):
-    print('  ' + f'_{rows - (rows)}_' + f' _{rows - (rows - 1)}_ ' + f' _{rows - (rows - 2)}_ ' + f' _{rows - (rows - 3)}_ ' + f' _{rows - (rows - 4)}_ ')
+    print(' ', end=" ")
+    print(f'{nums_top(rows)}')
 
     for row, x in zip(board, range(rows)):
         print(f'{x}| ' + '   '.join(row) + ' |')
@@ -131,27 +145,24 @@ def print_board2(board, rows):
             if board[x][y] == "#":
                 board[x][y] = "@"
 
-        # elif board[x][y] == ".":
-        #     hide_ships(board, rows)  
-
     print("The is print_board2") 
-    
-    print('  ' + f'_{rows - (rows)}_' + f' _{rows - (rows - 1)}_ ' + f' _{rows - (rows - 2)}_ ' + f' _{rows - (rows - 3)}_ ' + f' _{rows - (rows - 4)}_ ')
+
+    print(' ', end=" ")
+    print(f'{nums_top(rows)}')
 
     for row, x in zip(board, range(rows)):
         print(f'{x}| ' + '   '.join(row) + ' |')
     print(' ' + ' ‾' * rows * 2)
-
+    
+def nums_top(rows):
     for x in range(rows):
-        for y in range(rows):
-            if board[x][y] == "@":
-                board[x][y] = "#"
+        print(f'_{rows - (rows - x)}_', end=" ")
+    return ''
 
-# def hide_ships(board, x, y):
-#     if board[x][y] == "#":
-#         board[x][y] = "."
-#     elif board[x][y] == ".":
-#         hide_ships(board, rows)
+    # for x in range(rows):
+    #     for y in range(rows):
+    #         if board[x][y] == ".":
+    #             board[x][y] = "#"
 
 
 
