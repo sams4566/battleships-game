d83054662813200a1734411556025b07628a1380
sams4566 <sams4566@gmail.com>
2021-10-13 17:41:26 +0000

    Add the words for the README file

--- README.md
+++ README.md
@@ -1,31 +1,99 @@
-![CI logo](https://codeinstitute.s3.amazonaws.com/fullstack/ci_logo_small.png)
+# Battleships Game
 
-Welcome sams4566,
+The Battleships Game is an online version of the popular board game, Battleships, where two opponents go head to head to try and sink the other's ships. The game is set up so that each opponent can see their ships but cannot see the others. Each player enters a coordinate and the opponent lets them know if they have ‘hit’ a ship, ‘missed’ a ship or if they have already mentioned that coordinate. 
 
-This is the Code Institute student template for deploying your third portfolio project, the Python command-line project. The last update to this file was: **August 17, 2021**
+For this online version the user will have the opportunity to play against the computer and select different game board sizes to increase difficulty and the length of the game. The game is targeted at people who enjoy the original board game and would like to practise against the computer.
 
-## Reminders
+The live link to this game is below:
+#################Battleships Game#########################
 
-* Your code must be placed in the `run.py` file
-* Your dependencies must be placed in the `requirements.txt` file
-* Do not edit any of the other files or your code may not deploy properly
 
-## Creating the Heroku app
+![Am I Responsive Mockup](https://github.com/sams4566/rock-paper-scissors-lizard-spock/blob/main/media/am-i-responsive.jpg)
 
-When you create the app, you will need to add two buildpacks from the _Settings_ tab. The ordering is as follows:
+## Features
+- __Start Game__
 
-1. `heroku/python`
-2. `heroku/nodejs`
+  - The game starts off by asking the user to enter their name and then choose a game board size. 
+  - They have the option of choosing three board sizes (either 3x3, 4x4 or 5x5).
+  - If the user enters a number that is not 3, 4 or 5 an error message will appear and ask the user to enter a correct value.
 
-You must then create a _Config Var_ called `PORT`. Set this to `8000`
+![Start Game](https://github.com/sams4566/rock-paper-scissors-lizard-spock/blob/main/media/header.jpg)
 
-If you have credentials, such as in the Love Sandwiches project, you must create another _Config Var_ called `CREDS` and paste the JSON into the value field.
+- __Game Boards__
 
-Connect your GitHub repository and deploy as normal.
+  - The user is then presented with two boards. The first board is the users with ships randomly assigned to it and the second board is the computer’s with no ships visible. 
+  - A key is also presented to the user so they are aware of what the different symbols mean along with the format of the coordinates.
+  - Both boards are clearly labeled and have numbers along the top and left of the board to help the user pick their coordinates.
 
-## Constraints
 
-The deployment terminal is set to 80 columns by 24 rows. That means that each line of text needs to be 80 characters or less otherwise it will be wrapped onto a second line.
+![Game Boards](https://github.com/sams4566/rock-paper-scissors-lizard-spock/blob/main/media/game-options.jpg)
 
------
-Happy coding!
+- __Choosing Coordinates__
+  - The user is then prompted to choose a row and then a column to pin-point a dot (‘.’) on the computer’s board. 
+  - If the user enters a value outside of the numbers on the board the user will receive an error message and will be prompted to enter another coordinate. 
+  - The user will also be prompted to enter a new coordinate if they have already entered that coordinate.
+
+![Choosing Coordinates](https://github.com/sams4566/rock-paper-scissors-lizard-spock/blob/main/media/starting-the-game.jpg)
+
+- __Scoring__
+  - Once the user has entered a valid coordinate they are presented with both boards again. 
+  - The user's board has either a ‘X’ (hit) or a ‘~’ (miss) at the position of the computer's chosen coordinate. This coordinate is also printed out above the board to make it clear for the user the exact position.
+  - The computer's board also has either a ‘X’ (hit) or a ‘~’ (miss) at the position of the user’s chosen coordinate. The user’s coordinate is printed above the computer's board.
+  - The scores for both users are printed out below the computer's board. After every ‘hit’ the score is increased by 1.
+  - The user and computer both continue to pick coordinates until all the ships are ‘hit’.
+
+
+![Scoring](https://github.com/sams4566/rock-paper-scissors-lizard-spock/blob/main/media/game-area.jpg)
+
+- __End of Game__
+  - Once one or both players ‘hit’ all of their opponents ships a message declaring the winner appears at the bottom. 
+  - If the game is a draw, the message declares that both players have lost as there are no ships left.
+  - The game automatically restarts when the winner/losers has been announced.
+
+
+![End of Game](https://github.com/sams4566/rock-paper-scissors-lizard-spock/blob/main/media/collecting-points.jpg)
+
+- __Future features__
+  - Future features would include playing against a friend on another device and adding in the positions of your own ships. I would also like to add in an option where the user can choose a number of different game board shapes.
+
+## Data Model
+  - The below diagram shows how I set out my project before I started coding. This allowed me to work out where to start and what the different parts of the game would be. 
+  - I started off the project by getting the name and board size as both of these variables would be filtered through to other functions. 
+  - I then created the boards so that I could then work on modifying them.
+  - The run_game function is the main function where all the other functions return back to. Having the ‘for’ loop in this function allowed the game to continue in a circular motion like the diagram below.
+
+![Data Model](https://github.com/sams4566/rock-paper-scissors-lizard-spock/blob/main/media/collecting-points.jpg)
+
+## Testing
+  - I have tested the code thoroughly on Heroku and the terminal on Gitpod and there are no issues or errors.
+  - I have added code that prevents the game from crashing if the user inputs a value that cannot be processed.
+No errors were returned when passing through PEP8 Online################
+
+### Bugs
+#### Solved Bugs 
+
+- I had an issue where a ValueError was crashing the terminal when I inputted the ‘rows’ value as either the ‘row’ or ‘column’ value of the coordinate. Due to the maximum inputtable coordinate value being ‘rows - 1’ (due to the first coordinate starting at ‘0’) I had to change the correct_input functions ‘user_row > rows’ to ‘user_row >= rows’ as shown below: 
+
+  ![Bug1](https://github.com/sams4566/rock-paper-scissors-lizard-spock/blob/main/media/bug1.jpg)
+
+- I had an ‘or’ between SCORE1 and SCORE2 which meant that instead of the winner being announced the draw statement was announced. I changed the ‘or’ to an ‘and’ which resolved the issue as shown below:
+
+  ![Bug2](https://github.com/sams4566/rock-paper-scissors-lizard-spock/blob/main/media/bug2.jpg)
+
+#### Unfixed Bugs
+- No unfixed bugs
+
+## Deployment
+This project was deployed using Code Institute’s mock terminal for Heroku. Below are the steps for deployment:
+  - Fork or clone this repository 
+  - Go onto Heroku and click ‘Create new app’
+  - You then need to add two buildpacks from the Settings tab. The ordering is as follows:
+    1. `heroku/python`
+    2. `heroku/nodejs`
+  - You must then create a _Config Var_ called `PORT`. Set this to `8000`
+  - After link the Heroku app to this repository on GitHub
+  - Click on ‘Deploy’
+
+## Credits
+  - The code for learning how to print a loop horizontally was from - https://dev.to/theoretician/how-to-write-for-loops-horizontally-3h70 
+  - For learning how to combine two for loops together in a zip I used the following website - https://realpython.com/python-zip-function/ 
--- run.py
+++ run.py
@@ -1,22 +1,24 @@
 import random
 
+
 def start_game():
     """
     Start the game by collecting the users name.
     """
-    print('-' * 36)
+    print("-" * 36)
     print("Welcome to Battleships!")
-    print('-' * 36)
+    print("-" * 36)
     user_name = input("Please enter your name:\n")
     run_game(user_name)
 
+
 def run_game(user_name):
     """
-    Once the number of rows is returned and the players boards are 
-    created, a 'for' loop is created to facilitate the user and 
+    Once the number of rows is returned and the players boards are
+    created, a 'for' loop is created to facilitate the user and
     computer's choices until all the ships are sunk.
 
-    Once a winner is established a message is produced and the game 
+    Once a winner is established a message is produced and the game
     restarts.
     """
     rows = no_of_rows()
@@ -28,20 +30,24 @@ def run_game(user_name):
     print("X = Hit")
     print("~ = Miss")
     print("Coordinate = (row, column)")
-    print('-' * 36)
+    print("-" * 36)
     SCORE1 = 0
     SCORE2 = 0
     user_row1 = 0
     user_column1 = 0
     user_row2 = 0
     user_column2 = 0
-    
-    for x in range(100):
-        board_one, SCORE2, user_row2, user_column2 = computer_choice(board_one, rows, columns, SCORE2)
-        board_two, SCORE1, user_row1, user_column1 = users_choice(board_two, rows, SCORE1)
 
-        print('-' * 36)
-        print(f"{user_name}\'s board")
+    for x in range(100):
+        board_one, SCORE2, user_row2, user_column2 = computer_choice(
+            board_one, rows, columns, SCORE2
+        )
+        board_two, SCORE1, user_row1, user_column1 = users_choice(
+            board_two, rows, SCORE1
+        )
+
+        print("-" * 36)
+        print(f"{user_name}'s board")
         print(f"Computer shot at ({user_row2}, {user_column2})")
         print_user_board(board_one, rows)
 
@@ -49,22 +55,21 @@ def run_game(user_name):
         print(f"{user_name} shot at ({user_row1}, {user_column1})")
         print_computer_board(board_two, rows)
 
-        print('-' * 36)
-        print(f"{user_name}\'s Score: {SCORE1}/{rows * 2 - 2} ships sunk")
+        print("-" * 36)
+        print(f"{user_name}'s Score: {SCORE1}/{rows * 2 - 2} ships sunk")
         print(f"Computer's score: {SCORE2}/{rows * 2 - 2} ships sunk")
-        print('-' * 36)
+        print("-" * 36)
 
         if SCORE1 == rows * 2 - 2 and SCORE2 == rows * 2 - 2:
             print("There is no winner, both have been defeated!")
             start_game()
-
         if SCORE1 == rows * 2 - 2:
             print("Congratulations, you have defeated the enemy!")
             start_game()
-
         if SCORE2 == rows * 2 - 2:
             print("Game over, you have lost the battle!")
             start_game()
+
 
 def no_of_rows():
     """
@@ -73,35 +78,33 @@ def no_of_rows():
     to the run_game function.
     """
     while True:
-        rows1 = input("Board size: choose a number between 3 - 5:\n")
-       
+        rows1 = input("Board size: choose 3, 4 or 5:\n")
+
         if check_rows_input(rows1):
             break
-        
     rows1 = int(rows1)
     return rows1
+
 
 def check_rows_input(rows1):
     """
     Error messages are provided in the console if the inputed entries
     are not between 3 and 5.
-    """             
+    """
     try:
         if int(rows1) < 3 or int(rows1) > 5:
-            raise ValueError(
-                f"Invalid entry"
-            )
+            raise ValueError(f"Invalid entry")
     except ValueError as e:
         print(f"Invalid entry")
         return False
+    return True
 
-    return True            
 
 def user_board(rows, columns, user_name):
     """
-    The user's board is created using a 'for' loop and the 
-    ships are added to the board through the calculate_ships 
-    function. The user's board is then returned to the 
+    The user's board is created using a 'for' loop and the
+    ships are added to the board through the calculate_ships
+    function. The user's board is then returned to the
     run_game function.
     """
     game_board = []
@@ -112,16 +115,17 @@ def user_board(rows, columns, user_name):
         game_board.append(game_board_row)
     for x in range(rows * 2 - 2):
         calculate_ships(game_board, rows, columns)
-    print('-' * 36)
-    print(f"{user_name}\'s board")
+    print("-" * 36)
+    print(f"{user_name}'s board")
     print_user_board(game_board, rows)
     return game_board
 
+
 def computer_board(rows, columns):
     """
-    The computer's board is created using a 'for' loop and the 
-    ships are added to the board through the calculate_ships 
-    function. The computer's board is then returned to the 
+    The computer's board is created using a 'for' loop and the
+    ships are added to the board through the calculate_ships
+    function. The computer's board is then returned to the
     run_game function.
     """
     game_board = []
@@ -136,6 +140,7 @@ def computer_board(rows, columns):
     print_computer_board(game_board, rows)
     return game_board
 
+
 def calculate_ships(game_board, rows, columns):
     """
     A ships ('#') is added to the game board and the new game board
@@ -149,15 +154,16 @@ def calculate_ships(game_board, rows, columns):
         calculate_ships(game_board, rows, columns)
     return game_board
 
+
 def computer_choice(board_one, rows, columns, SCORE2):
     """
-    The computer keeps choosing a random number until the coordinates 
-    haven't been entered before. Once the 'while' look is broken, 
-    a hit ('X') or miss ('~') is added to the users board and 
+    The computer keeps choosing a random number until the coordinates
+    haven't been entered before. Once the 'while' look is broken,
+    a hit ('X') or miss ('~') is added to the users board and
     subsequently returned to the 'for' loop in the run_game function.
 
-    If the computers choice is a hit ('X') the computers score is 
-    incremented by 1 and returned to the 'for' loop in the run_game 
+    If the computers choice is a hit ('X') the computers score is
+    incremented by 1 and returned to the 'for' loop in the run_game
     function.
     """
     while True:
@@ -168,24 +174,23 @@ def computer_choice(board_one, rows, columns, SCORE2):
             break
         elif board_one[int(user_row2)][int(user_column2)] == "#":
             break
-
     if board_one[user_row2][user_column2] == ".":
         board_one[user_row2][user_column2] = "~"
-        
-    elif board_one[user_row2][user_column2] == '#':
+    elif board_one[user_row2][user_column2] == "#":
         board_one[user_row2][user_column2] = "X"
         SCORE2 = SCORE2 + 1
     return board_one, SCORE2, user_row2, user_column2
 
+
 def users_choice(board_two, rows, SCORE1):
     """
-    The user keeps choosing a number until the coordinates 
-    haven't been entered before. Once the 'while' look is broken, 
-    a hit ('X') or miss ('~') is added to the computers board and 
+    The user keeps choosing a number until the coordinates
+    haven't been entered before. Once the 'while' look is broken,
+    a hit ('X') or miss ('~') is added to the computers board and
     subsequently returned to the 'for' loop in the run_game function.
 
-    If the users choice is a hit ('X') the users score is 
-    incremented by 1 and returned to the 'for' loop in the run_game 
+    If the users choice is a hit ('X') the users score is
+    incremented by 1 and returned to the 'for' loop in the run_game
     function.
     """
     while True:
@@ -202,18 +207,17 @@ def users_choice(board_two, rows, SCORE1):
                     break
                 elif board_two[int(user_row1)][int(user_column1)] == "#":
                     break
-
     user_row1 = int(user_row1)
     user_column1 = int(user_column1)
 
     if board_two[user_row1][user_column1] == ".":
         board_two[user_row1][user_column1] = "~"
-        
-    elif board_two[user_row1][user_column1] == '#':
+    elif board_two[user_row1][user_column1] == "#":
         board_two[user_row1][user_column1] = "X"
         SCORE1 = SCORE1 + 1
     return board_two, SCORE1, user_row1, user_column1
-        
+
+
 def correct_input(user_row, rows):
     """
     Error messages are provided if the user enters a value
@@ -221,42 +225,40 @@ def correct_input(user_row, rows):
     """
     try:
         if int(user_row) >= rows or int(user_row) < 0:
-            raise ValueError(
-                f"Entries must be between 0 - {rows - 1}"
-            )
+            raise ValueError(f"Entries must be between 0 - {rows - 1}")
     except ValueError as e:
         print(f"Entries must be between 0 - {rows - 1}")
         return False
-    
     return True
+
 
 def print_user_board(board, rows):
     """
-    The users board is printed out in a readable way with spaces between the 
+    The users board is printed out in a readable way with spaces between the
     dots and numbers along the side.
     """
-    print(' ', end=" ")
-    print(f'{nums_top(rows)}')
-    
+    print(" ", end=" ")
+    print(f"{nums_top(rows)}")
+
     for row, x in zip(board, range(rows)):
-        print(f'{x}| ' + '   '.join(row) + ' |')
+        print(f"{x}| " + "   ".join(row) + " |")
+    print(" " + " ‾" * rows * 2)
 
-    print(' ' + ' ‾' * rows * 2)
 
-def print_computer_board(board, rows): 
+def print_computer_board(board, rows):
     """
     The computers board is printed out in a readable way with spaces between
-    the dots and numbers along the side. The ships are hidden by replacing 
+    the dots and numbers along the side. The ships are hidden by replacing
     the '#' with a '.'.
     """
-    print(' ', end=" ")
-    print(f'{nums_top(rows)}')
+    print(" ", end=" ")
+    print(f"{nums_top(rows)}")
 
     for row, x in zip(board, range(rows)):
         row_copy = [row_char.replace("#", ".") for row_char in row]
-        print(f'{x}| ' + '   '.join(row_copy) + ' |')
+        print(f"{x}| " + "   ".join(row_copy) + " |")
+    print(" " + " ‾" * rows * 2)
 
-    print(' ' + ' ‾' * rows * 2)
 
 def nums_top(rows):
     """
@@ -264,7 +266,8 @@ def nums_top(rows):
     between them.
     """
     for x in range(rows):
-        print(f'_{rows - (rows - x)}_', end=" ")
-    return ''
+        print(f"_{rows - (rows - x)}_", end=" ")
+    return ""
+
 
 start_game()
